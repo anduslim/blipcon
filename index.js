1223d012c1989c346ac78ec1fad4bd9f69671bbb
Andus <yuncai83@gmail.com>
2017-03-03 11:53:22 +0800

    Output to json files

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var EddystoneBeaconScanner = require('eddystone-beacon-scanner');
+var bunyan = require('bunyan');
+var CurrentPath = __dirname;
 
 EddystoneBeaconScanner.on('found', function(beacon) {
   console.log('found Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
 });
 
 EddystoneBeaconScanner.on('updated', function(beacon) {
-  console.log('updated Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  var incoming = JSON.stringify(beacon, null, 2);
+  var LoggingFile = CurrentPath + '/' + beacon.instance + '.json';
+  var log = bunyan.createLogger({
+    name: beacon.instance,
+    streams: [{
+        path: LoggingFile,
+    }]
+  });
+  log.info(incoming);
+  console.log('updated Eddystone Beacon:\n', incoming);
 });
 
 EddystoneBeaconScanner.on('lost', function(beacon) {
@@ -13,3 +24,4 @@ EddystoneBeaconScanner.on('lost', function(beacon) {
 });
 
 EddystoneBeaconScanner.startScanning(true);
+
